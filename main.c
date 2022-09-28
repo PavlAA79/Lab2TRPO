e95285023ee7390ddec32867e5992a971af5b95a
Pavlovskaya <paa4851@gmail.com>
2022-09-28 14:29:21 +0300

    new file and chsnges in main

--- main.c
+++ main.c
@@ -9,5 +9,5 @@ int main(int argc, char **argv)
     printf("Now enter your last name: "); 
     gets(last); /* buffer overflow? what's that? */ 
     printf("Hello %s %s!\n", first, last); 
-    return 0; 
+    return 0;  //123
 }
