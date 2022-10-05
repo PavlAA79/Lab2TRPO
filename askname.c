ee77ee205971890d4fd59cd99b0af3cc9b1bfbf6
Liam172 <114081591+Liam172@users.noreply.github.com>
2022-10-05 14:14:15 +0300

    Update askname.c

--- askname.c
+++ askname.c
@@ -6,5 +6,5 @@ void askname(char *first, char *last)
     fgets(first, 255, stdin); 
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
     printf("Now enter your last name: "); 
-    gets(last); /* buffer overflow? what's that? */ 
+    gets(last); /* buffer overflow? what's that? */ot
 }
